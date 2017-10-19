e23643d2294bc2a9de5b3c6c9cd7aac5e3f92870
cboutaud <charles.boutaud@gmail.com>
2017-10-19 17:00:38 +0100

    comments, output headers

--- matches.py
+++ matches.py
@@ -2,25 +2,27 @@ from fuzzywuzzy import process
 from fuzzywuzzy import fuzz
 import csv
 
+
+# put all recipients from the csv into a python list 
 inputfile = open("all_recipients.csv", "rU")
 csv_reader = csv.reader(inputfile)
 next(csv_reader) # skip headers
-recipients = [row[0] for row in csv_reader]
+recipients = [row[0] for row in csv_reader] 
 
+
+# create a file to write the output in
 outputfile = open("matches.csv", "wb")
 csv_writer = csv.writer(outputfile)
-csv_writer.writerow(["la", "best", "second", "third"])
+csv_writer.writerow(["la", "type", "county", "best_match", "second_match", "third_match"])
 
+
+# for every local authority in England, find the three best matches in the list of recipients
 inputfile = open("la.csv", "rU")
 csv_reader = csv.reader(inputfile)
 next(csv_reader) # skip headers
-for index, row in enumerate(csv_reader):
-	# if index >= 10:
-	# 	break
+for row in csv_reader:
 	la = row[0]
 	matches = process.extract(la, recipients, limit=3, scorer=fuzz.token_set_ratio)
 	matches = [match[0] for match in matches]
 	row.extend(matches)
 	csv_writer.writerow(row)
-	
-
