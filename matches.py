6d4898a063a2e6c4c1a550447b53e1d5545190d9
cboutaud <charles.boutaud@gmail.com>
2017-10-30 13:05:43 +0000

    improved comments

--- matches.py
+++ matches.py
@@ -16,10 +16,12 @@ csv_writer = csv.writer(outputfile)
 csv_writer.writerow(["la", "type", "county", "best_match", "second_match", "third_match"])
 
 
-# for every local authority in England, find the three best matches in the list of recipients
+# for every local authority in England
+# find the three best matches in the list of recipients
+# write the results in the output file
 inputfile = open("la.csv", "rU")
 csv_reader = csv.reader(inputfile)
-next(csv_reader) # skip headers
+next(csv_reader)
 for row in csv_reader:
 	la = row[0]
 	matches = process.extract(la, recipients, limit=3, scorer=fuzz.token_set_ratio)
--- verif-match.py
+++ verif-match.py
@@ -1,19 +1,35 @@
 import csv
 
+# this file was written to check if a district
+# that hadn't received funding directly
+# might have received funding through their council
+# so logically we want to ask
+# a. did the local authority received funding directly
+# b. if not is it a district
+# c. if yes did it's county received funding
+# d. if yes indicate a match as "via [name of county]"
+
+# puts all local authority in a python dictionary
+# with their correct match after manual cleaning
 inputfile = open("match-clean.csv", "rU")
 csv_reader = csv.reader(inputfile)
 headers = next(csv_reader)
 matches = {row[0]: row[1] for row in csv_reader}
 
+# loads information about all local authorities
+# like the type of authority and county authority
 inputfile = open("la.csv", "rU")
 csv_reader = csv.reader(inputfile)
 next(csv_reader)
 las = {row[0]: row[1:] for row in csv_reader}
 
+# create a file to write the output in
 outputfile = open("covered.csv", "wb")
 csv_writer = csv.writer(outputfile)
 csv_writer.writerow(["la", "covered-by"])
 
+# this is where we execute the logical operation
+# mentioned at the top of the file
 inputfile = open("match-clean.csv", "rU")
 csv_reader = csv.reader(inputfile)
 next(csv_reader)
@@ -24,6 +40,7 @@ for row in csv_reader:
 		if las[la][0] == "Two-Tier District":
 			if matches[las[la][1]] != "":
 				row[1] = "via " + matches[las[la][1]]
-	# We don't want the counties
+	# we don't want the two-tier non metropolitan counties
+	# to avoid duplicate coverage with district
 	if las[la][0] != "Two-Tier County": 
 		csv_writer.writerow(row) 
